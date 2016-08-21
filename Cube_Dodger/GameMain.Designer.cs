a2d4eae1a90c3c94360f6eed5c46a37f4cab0186
Szymon Kocur <skocur10@gmail.com>
2016-08-21 18:35:53 +0200

    Add all my files

---
+++ Cube_Dodger/GameMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Cube_Dodger
+{
+    partial class GameMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameMain));
+            this.scoreTxt_label = new System.Windows.Forms.Label();
+            this.scoreDynamic_label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerEnemy = new System.Windows.Forms.Timer(this.components);
+            this.gameOverTxt_label = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.dubstepTxt_label = new System.Windows.Forms.Label();
+            this.timerEnemy2 = new System.Windows.Forms.Timer(this.components);
+            this.timerEnemy3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // scoreTxt_label
+            // 
+            this.scoreTxt_label.AutoSize = true;
+            this.scoreTxt_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreTxt_label.Location = new System.Drawing.Point(13, 13);
+            this.scoreTxt_label.Name = "scoreTxt_label";
+            this.scoreTxt_label.Size = new System.Drawing.Size(107, 31);
+            this.scoreTxt_label.TabIndex = 0;
+            this.scoreTxt_label.Text = "Score: ";
+            // 
+            // scoreDynamic_label
+            // 
+            this.scoreDynamic_label.AutoSize = true;
+            this.scoreDynamic_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreDynamic_label.Location = new System.Drawing.Point(113, 13);
+            this.scoreDynamic_label.Name = "scoreDynamic_label";
+            this.scoreDynamic_label.Size = new System.Drawing.Size(30, 31);
+            this.scoreDynamic_label.TabIndex = 1;
+            this.scoreDynamic_label.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timerMoving);
+            // 
+            // timerEnemy
+            // 
+            this.timerEnemy.Enabled = true;
+            this.timerEnemy.Interval = 1;
+            this.timerEnemy.Tick += new System.EventHandler(this.timerMoving_Enemy);
+            // 
+            // gameOverTxt_label
+            // 
+            this.gameOverTxt_label.AutoSize = true;
+            this.gameOverTxt_label.Font = new System.Drawing.Font("Roboto Slab", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameOverTxt_label.Location = new System.Drawing.Point(84, 215);
+            this.gameOverTxt_label.Name = "gameOverTxt_label";
+            this.gameOverTxt_label.Size = new System.Drawing.Size(210, 43);
+            this.gameOverTxt_label.TabIndex = 2;
+            this.gameOverTxt_label.Text = "GAME OVER";
+            this.gameOverTxt_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gameOverTxt_label.Visible = false;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(92, 270);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(195, 270);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit game";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Visible = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // dubstepTxt_label
+            // 
+            this.dubstepTxt_label.AutoSize = true;
+            this.dubstepTxt_label.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dubstepTxt_label.Location = new System.Drawing.Point(27, 97);
+            this.dubstepTxt_label.Name = "dubstepTxt_label";
+            this.dubstepTxt_label.Size = new System.Drawing.Size(336, 45);
+            this.dubstepTxt_label.TabIndex = 5;
+            this.dubstepTxt_label.Text = "DUBSTEP MODE!!!!";
+            this.dubstepTxt_label.Visible = false;
+            // 
+            // timerEnemy2
+            // 
+            this.timerEnemy2.Enabled = true;
+            this.timerEnemy2.Interval = 1;
+            this.timerEnemy2.Tick += new System.EventHandler(this.timerMoving_Enemy2);
+            // 
+            // timerEnemy3
+            // 
+            this.timerEnemy3.Enabled = true;
+            this.timerEnemy3.Interval = 1;
+            this.timerEnemy3.Tick += new System.EventHandler(this.timerMoving_Enemy3);
+            // 
+            // GameMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 662);
+            this.Controls.Add(this.dubstepTxt_label);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.gameOverTxt_label);
+            this.Controls.Add(this.scoreDynamic_label);
+            this.Controls.Add(this.scoreTxt_label);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(400, 700);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 700);
+            this.Name = "GameMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game by SKocur";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameMain_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameMain_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreTxt_label;
+        private System.Windows.Forms.Label scoreDynamic_label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerEnemy;
+        private System.Windows.Forms.Label gameOverTxt_label;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label dubstepTxt_label;
+        private System.Windows.Forms.Timer timerEnemy2;
+        private System.Windows.Forms.Timer timerEnemy3;
+    }
+}
+
---
+++ Cube_Dodger/GameMain.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cube_Dodger
+{
+    public partial class GameMain : Form
+    {
+        enum PlayerPosition
+        {
+            Left, Right
+        }
+
+        private int score;
+
+        /* Localization (loc) of player */
+        private int locXplayer;
+        private int locYplayer;
+
+        /* Position (pos) of enemy (cube) */
+        private int posXenemy1;
+        private int posYenemy1;
+        private int posXenemy2;
+        private int posYenemy2;
+        private int posXenemy3;
+        private int posYenemy3;
+        private int posXenemy4;
+        private int posYenemy4;
+
+        private PlayerPosition playerPos;
+
+        public GameMain()
+        {
+            InitializeComponent();
+
+            Random randomPosEnemy = new Random();
+            Cursor.Hide();
+            score = 0;
+            this.BackColor = System.Drawing.Color.White;
+
+            /* Center of screen
+            400 is width of playground
+            50 is width of player's cube
+            so
+            400 / 2 = 200
+            50 / 2 = 25
+            200 - 25 = 175
+            */
+            locXplayer = 175;
+            locYplayer = 550;
+
+            /* Positions of enemies */
+            posXenemy1 = randomPosEnemy.Next(10, 50);
+            posYenemy1 = randomPosEnemy.Next(50, 150);
+            posXenemy2 = randomPosEnemy.Next(150, 200);
+            posYenemy2 = randomPosEnemy.Next(50, 150);
+            posXenemy3 = randomPosEnemy.Next(200, 300);
+            posYenemy3 = randomPosEnemy.Next(50, 150);
+            posXenemy4 = randomPosEnemy.Next(200, 280);
+            posYenemy4 = randomPosEnemy.Next(50, 150);
+
+        }
+
+        private void timerMoving_Enemy(object sender, EventArgs e)
+        {
+            Random randomPos = new Random();
+            if (posYenemy1 < 700)
+            {
+                posYenemy1 += 4;
+                /* Better is Rectangle.Intersect method */
+                /*if((locYplayer >= posYenemy1 && locYplayer <= (posYenemy1 + sizeYenemy1)) && ((locXplayer + 50) >= posXenemy1 && (locXplayer + 50) <= (posXenemy1 + sizeXenemy1)) || locXplayer >= posXenemy1 && locXplayer <= (posXenemy1 + sizeXenemy1))
+                {
+                    //some code;
+                } */
+            }
+            else if (posYenemy1 >= 700)
+            {
+                score += 1;
+                posYenemy1 = randomPos.Next(50, 200);
+                scoreDynamic_label.Text = score.ToString();
+            }
+            Invalidate();
+        }
+
+        private void timerMoving_Enemy2(object sender, EventArgs e)
+        {
+            Random randomPos = new Random();
+            if (posYenemy2 < 700)
+            {
+                posYenemy2 += 4;
+            }
+            else if (posYenemy2 >= 700)
+            {
+                score += 1;
+                posYenemy2 = randomPos.Next(50, 200);
+                scoreDynamic_label.Text = score.ToString();
+            }
+            Invalidate();
+        }
+
+        private void timerMoving_Enemy3(object sender, EventArgs e)
+        {
+            Random randomPos = new Random();
+            if (posYenemy3 < 700)
+            {
+                posYenemy3 += 4;
+            }
+            else if (posYenemy3 >= 700)
+            {
+                score += 1;
+                posYenemy3 = randomPos.Next(50, 200);
+                scoreDynamic_label.Text = score.ToString();
+            }
+            Invalidate();
+        }
+
+        private void timerMoving(object sender, EventArgs e)
+        {
+            Random randomColor = new Random();
+
+            if(playerPos == PlayerPosition.Right)
+            {
+                if (locXplayer <= 335) /* 335 is X localization of right border */
+                {
+                    locXplayer += 3; /* How fast our player is */
+                    dubstepTxt_label.Visible = false;
+                }
+                else
+                {
+                    dubstepTxt_label.Visible = true;
+                    this.BackColor = Color.FromArgb(randomColor.Next(150, 255), randomColor.Next(150, 255), randomColor.Next(150, 255));
+                }
+            }
+            else if(playerPos == PlayerPosition.Left)
+            {
+                if (locXplayer >= 0) /* 0 is X localization of left border */
+                {
+                    locXplayer -= 3; /* How fast our player is */
+                    dubstepTxt_label.Visible = false;
+                }
+                else
+                {
+                    dubstepTxt_label.Visible = true;
+                    this.BackColor = Color.FromArgb(randomColor.Next(150, 255), randomColor.Next(150, 255), randomColor.Next(150, 255));
+                }
+            }       
+            
+            Invalidate(); /* Start from beginning */
+        }
+
+        private void GameMain_Paint(object sender, PaintEventArgs e)
+        {
+            Random randomPos = new Random();
+
+            Rectangle player = new Rectangle(locXplayer, locYplayer, 50, 50);
+            Rectangle enemy1 = new Rectangle(posXenemy1, posYenemy1, 50, 70);
+            Rectangle enemy2 = new Rectangle(posXenemy2, posYenemy2, 50, 50);
+            Rectangle enemy3 = new Rectangle(posXenemy3, posYenemy3, 50, 60);
+
+            //e.Graphics.FillRectangle(Brushes.Blue, locXplayer, locYplayer, 50, 50); /* Player's cube */
+            e.Graphics.FillRectangle(Brushes.Blue, player);
+            e.Graphics.FillRectangle(Brushes.Red, enemy1);
+            e.Graphics.FillRectangle(Brushes.Red, enemy2);
+            e.Graphics.FillRectangle(Brushes.Red, enemy3);
+
+            if(player.IntersectsWith(enemy1))
+            {
+                EndMenu();
+            }
+            else if(player.IntersectsWith(enemy2))
+            {
+                EndMenu();
+            }
+            else if(player.IntersectsWith(enemy3))
+            {
+                EndMenu();
+            }
+        }
+
+        private void GameMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                playerPos = PlayerPosition.Right;
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                playerPos = PlayerPosition.Left;
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            GameMain formMain = new GameMain();
+            formMain.ShowDialog();
+
+            this.Close();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EndMenu()
+        {
+            btnQuit.Visible = true;
+            btnRestart.Visible = true;
+            Cursor.Show();
+            timer1.Enabled = false;
+            timerEnemy.Enabled = false;
+            timerEnemy2.Enabled = false;
+            timerEnemy3.Enabled = false;
+            gameOverTxt_label.Visible = true;
+        }
+
+
+    }
+}
